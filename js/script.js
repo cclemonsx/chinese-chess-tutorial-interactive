1ef8e913438c59e6ce71d7a78f55841d45c3814b
Cece Xue <cecileaxue@gmail.com>
2018-07-02 19:23:20 -0400

    positioned pieces, general moves for tutorial

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
+var genMoves=[
+    "down",
+    "right",
+    "left",
+    "up"
+]
 
+var moveCount=0
+
+$(document).ready(function(highlightLegal){
+    $(".highlightThis").mouseover(function(){
+    $(this).toggleClass()
+})
+})
+// function highlightLegal() {
+//         $(this).toggleClass(".highlightTop .highlightBottom", addOrRemove);
+//     }
+function moveSelection() {
+    switch (genMoves[moveCount]){
+        case "down":
+        console.log("move general down");
+        $("#genRed").animate({
+            "top": "100%"}, "slow")
+
+        break;
+        case "right":
+        console.log("move general right");
+        $("#genRed").animate({
+            "right": "-100%"}, "slow")
+
+        break;
+        case "left":
+        console.log("move general left");
+        $("#genRed").animate({
+            "right": "0"}, "slow")
+        break;
+        case "up":
+        console.log("move general up");
+        $("#genRed").animate({"top": "0"}, "slow")
+        break;
+    }
+    moveCount++
+    if (moveCount > 3)
+        {moveCount=0}
+}
